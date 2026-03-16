ab2c66058ae15a08d675ff5e0b76ce2b7c0255d0
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:21 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,20 +10,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoLibraryCheck = (import "${substrate}/lib/go-library-check.nix").mkGoLibraryCheck;
-    in {
-      checks.default = mkGoLibraryCheck pkgs {
-        pname = "go-kmip";
-        version = "0.0.0-dev";
-        src = self;
-        vendorHash = "sha256-KemjY8oC4UmBhp79TF5st6PsoLq32edL9SaSK/L/FsA=";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "library";
+    pname = "go-kmip";
+    vendorHash = "sha256-KemjY8oC4UmBhp79TF5st6PsoLq32edL9SaSK/L/FsA=";
+    description = "Go implementation of a subset of the KMIP 1.4 protocol";
+    homepage = "https://github.com/pleme-io/go-kmip";
+  };
 }
